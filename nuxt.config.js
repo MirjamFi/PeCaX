01906ea274cd9eb5e963478dd686e8a4cc627d21
MirjamFi <figasch@informatik.uni-tuebingen.de>
2020-09-24 14:37:19 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,7 +89,7 @@ axios: {
 proxy: {
   '/clinvap': {target: 'http://nginx:80/',pathRewrite:{'^/clinvap/':''}},
   '/db': {target: 'http://arangodb:8529/',pathRewrite:{'^/db/':'/'}},
-  '/network': { target: 'http://sbml4j:8080/sbml4j/', pathRewrite: {'^/network/': ''}},
+  '/network': { target: 'http://sbml4j:8080/sbml4j/', pathRewrite: {'^/network/': '/'}},
   '/visualization':{target: 'http://biographvisart:3000/BioGraphVisart/', pathRewrite: {'^/visualization/': ''}}
 },
 
