9515da146632ae1aa34357b312e82dc2e02827f0
MirjamFi <figasch@informatik.uni-tuebingen.de>
2020-10-07 16:28:07 +0200

    My Account into dropdown

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="topnav" id ="nav">
-    <div class="dropdown topnav-left" id="navAbout">  
+    <div class="dropdown topnav-right" id="navAbout">  
       <button class="dropbtn"><b-icon data-html2canvas-ignore="true" icon="list"></b-icon>
         <div class="dropdown-content">
-          <a href="/about" class="page-link">About </a>
+          <a class="page-link" href="/profile">My Account</a>
+          <a class="page-link" href="/about" >About </a>
           <a class="page-link" href="/documentation">Documentation</a>
           <a class="page-link" href="/contact">Contact</a>
         </div>
@@ -12,14 +13,14 @@
     <div class="topnav-centered">
       <a class="page-link active border-0 border-dark" href="/">PeCaX</a>
     </div>
-    <div class="topnav-right" ref="account">
-       <a class="page-link border-0 border-dark" href="/profile">My Account</a>
+    <!-- <div class="topnav-right" ref="account"> -->
+       
       <!-- <button class="dropdown dropbtn">My Account<b-icon data-html2canvas-ignore="true" icon="list"></b-icon> -->
         <!-- <div class="dropdown-content"> -->
           <!-- <a href="/profile" class="page-link">My Account</a> -->
 <!--           <a class="page-link" href="/">Logout</a> -->
         <!-- </div> -->
       <!-- </button> -->
-    </div>    
+    <!-- </div>     -->
   </div>
 </template>
