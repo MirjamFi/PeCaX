69c3d50ed160d60da6437cf9961539ca497bf38f
MirjamFi <figasch@informatik.uni-tuebingen.de>
2020-12-02 16:22:10 +0100

    size of list icon

--- pages/start.vue
+++ pages/start.vue
@@ -26,7 +26,7 @@
 		      <a class="page-link active border-0 border-dark" href="/">PeCaX</a>
 		    </div>
 		    <div class="dropdown topnav-left" id="navAbout">  
-		      <button class="dropbtn"><b-icon data-html2canvas-ignore="true" icon="list"></b-icon>
+		      <button class="dropbtn"><b-icon data-html2canvas-ignore="true" icon="list" class="h3"></b-icon>
 		        <div class="dropdown-content">
 		          <a class="page-link" href="/profile">My Jobs</a>
 		          <a class="page-link" href="/about" >About </a>
